62720e9b78cfaa84c2810b8a18c736d137514ba0
Maghish <mail4mithran@gmail.com>
2023-12-29 20:56:10 +0530

    Added workflow + added back-end unit test cases

--- Pluto/Pluto_Backend/tests.py
+++ Pluto/Pluto_Backend/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import SampleModel
 
 # Create your tests here.
+class TestSampleModel(TestCase):
+
+    def setUp(self):
+        SampleModel.objects.create(sample_text="Hello world!")
+
+    def test_sample(self):
+        self.assertEqual(True, True)
