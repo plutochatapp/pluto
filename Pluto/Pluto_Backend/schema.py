f43f7792b9f3219e5e8c5c9403ec08e34f5ca6a1
Maghish <mail4mithran@gmail.com>
2023-12-31 20:44:43 +0530

    Updated the schema with the new models

--- Pluto/Pluto_Backend/schema.py
+++ Pluto/Pluto_Backend/schema.py
@@ -1,17 +1,35 @@
 import graphene
-from graphene_django import DjangoObjectType
+from graphene_django import DjangoObjectType, DjangoListField
 
-from .models import SampleModel
+from .models import *
 
-class SampleType(DjangoObjectType):
+class UserType(DjangoObjectType):
     class Meta:
-        model = SampleModel
-        fields = "__all__"
+        model = User
+        fields = ("id", "username", "email", "bio", "timestamp", "friends", "profile_picture")
+
+class MessageType(DjangoObjectType):
+    class Meta:
+        model = Message
+        fields = ("id", "author", "content", "reply", "timestamp")
+    
 
 class Query(graphene.ObjectType):
-    all_samples = graphene.List(SampleType)
+    all_users = graphene.List(UserType)
+    all_messages = graphene.List(MessageType)
+    user = graphene.Field(UserType, id=graphene.Int(), username=graphene.String(), email=graphene.String())
+    message = graphene.Field(MessageType, id=graphene.Int())
 
-    def resolve_all_samples(root, info):
-        return SampleModel.objects.all()
+    def resolve_all_users(root, info): return User.objects.all()
+    def resolve_all_messages(root, info): return Message.objects.all()
+    def resolve_user(root, info, id=None, username=None, email=None): 
+        if id: return User.objects.get(id=id)
+        if username: return User.objects.get(username=username)
+        if email: return User.objects.get(email=email)
+        return None
+    def resolve_message(root, info, id):
+        if id: return Message.objects.get(id=id)
+        return None
+    
     
 schema = graphene.Schema(query=Query)   
