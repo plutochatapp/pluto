23a30be982e9b74d2b2102a660c04907ed86c614
Maghish <mail4mithran@gmail.com>
2024-01-08 22:37:51 +0530

    Created new Navbar component and added main css file and other css files

--- Pluto-Frontend/src/components/App.jsx
+++ Pluto-Frontend/src/components/App.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from 'react'
 import { useQuery, gql } from '@apollo/client';
- 
 
-const query = gql`
-    query MyQuery {
-        allUsers {
-            username
-        }
-    }
-`;
+// const query = gql`
+//     query MyQuery {
+//         allUsers {
+//             username
+//         }
+//     }
+// `;
 
 function App() {
-    const { loading, error, data } = useQuery(query);
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error...</p>
-    else {
-        return <p>{data.allUsers[0].username}</p>
-    }
+    // const { loading, error, data } = useQuery(query);
+    // if (loading) return <p>Loading...</p>
+    // if (error) return <p>Error...</p>
+    // else {
+    //     return <p>{data.allUsers[0].username}</p>
+    // }
+
+    return 
+        <>
+        </>
 }
 
 export default App
--- Pluto-Frontend/src/main.jsx
+++ Pluto-Frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+import App from './components/App.jsx'
+import Navbar from './components/Navbar.jsx'
+
+import './css/main.css'
 
 let client = new ApolloClient({
     uri: 'http://127.0.0.1:8000/graphql/',
@@ -13,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
         <App />
+        <Navbar />
     </ApolloProvider>
   </React.StrictMode>,
 )
