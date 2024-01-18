52adc6b31a9d9e0cff640a36a38522a49aaaa65d
Maghish <mail4mithran@gmail.com>
2024-01-18 22:18:57 +0530

    Create Components for Login and Signup Form + Edited the tailwind config file

---
+++ Pluto-Frontend/src/components/Loginform.jsx
@@ -0,0 +1,14 @@
+import '../css/index.css'
+import React from 'react'
+
+function Loginform() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-modal-backdrop-color">
+        <div className="p-8 bg-navbar-color rounded-lg max-w-md my-52 m-auto">
+            <p className="text-white">Log In</p>
+        </div>
+    </div>
+  )
+}
+
+export default Loginform
--- Pluto-Frontend/tailwind.config.js
+++ Pluto-Frontend/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         colors: {
             'navbar-color': '#161616',
             'navbar-btn-color': '#252525',
+            'modal-backdrop-color': 'rgba(0, 0, 0, 0.5)',
         },
     },
   },
