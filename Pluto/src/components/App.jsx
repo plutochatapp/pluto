dbedb511b80770d4b33a7396c1b7efed96782b88
Maghish <mail4mithran@gmail.com>
2024-01-25 20:27:31 +0530

    Added Many Things

--- Pluto/src/components/App.jsx
+++ Pluto/src/components/App.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, Navigate, useRoutes } from 'react-router-dom';
+import { Routes, Route, Navigate, useRoutes } from 'react-router-dom'
 
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { app } from '../firebase.js'
+
 import { getAuth } from 'firebase/auth'
-
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+import Home from './Home.jsx'
+import Loginform from './Loginform.jsx'
+import Signupform from './Signupform.jsx'
+
  
-import Navbar from './Navbar.jsx'
-import Loginform from './Loginform.jsx';
-
-import firebaseConfig from '../../firebaseConfig.json'
-
-const app = initializeApp(firebaseConfig)
-
 const auth = getAuth(app)
-const firestore = getFirestore(app)
- 
+
 function DecideCurrentPage() {
-    return <Navigate to='/login' />   
+    const [user] = useAuthState(auth)
+
+    if (!
+        user) {
+        return <Navigate to="/login" />
+    }
+
+    else {
+        return <Home />
+    }
 }
 
 export default function App() {
@@ -29,11 +33,15 @@ export default function App() {
             children: [
                 {
                     index: true,
-                    element: <DecideCurrentPage />
+                    element: <DecideCurrentPage />,
                 },
                 {
                     path: 'login',
                     element: <Loginform />,
+                }, 
+                {
+                    path: 'signup',
+                    element: <Signupform />,
                 }
             ]
         }
--- Pluto/src/components/Signupform.jsx
+++ Pluto/src/components/Signupform.jsx
@@ -1,3 +1,27 @@
 import '../css/index.css'
-import { useState } from 'react'
 
+import { useState } from 'react'
+import { getAuth } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { app } from '../firebase.js'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const auth = getAuth(app)
+
+function Signupform() {
+
+    const [passwordShowing, setPasswordShowing] = useState(false)
+
+    const managePasswordFieldVisibility = () => {
+        if (!passwordShowing) {setPasswordShowing(true)}
+        if (passwordShowing) {setPasswordShowing(false)}
+    }
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Signupform
